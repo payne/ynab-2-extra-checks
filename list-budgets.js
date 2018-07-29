8198543e910cad9d213cd10edf0a8408970b9881
Matt Payne <Payne@MattPayne.org>
2018-07-29 16:32:41 -0500

    Demo program uses secret in config-ynab.js

---
+++ list-budgets.js
@@ -0,0 +1,12 @@
+const ynab = require("ynab");
+const token = require("./config-ynab");
+console.log(token.accessToken);
+const ynabAPI = new ynab.API(token.accessToken);
+
+(async function() {
+  const budgetsResponse = await ynabAPI.budgets.getBudgets();
+  const budgets = budgetsResponse.data.budgets;
+  for(let budget of budgets) {
+    console.log(`Budget Name: ${budget.name}`);
+  }
+})();
