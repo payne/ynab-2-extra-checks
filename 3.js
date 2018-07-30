5a3bbf9165522fcfee648edcc520f4e47c555c14
Matt Payne <Payne@MattPayne.org>
2018-07-29 21:08:28 -0500

    Example output of caterories json from 3.js

---
+++ 3.js
@@ -0,0 +1,43 @@
+async function getBudgetId(ynabAPI) {
+  const budgetsResponse = await ynabAPI.budgets.getBudgets();
+  const budgets = budgetsResponse.data.budgets;
+  let budgetId;
+  for(let budget of budgets) {
+		if ('Demo' === budget.name) {
+			budgetId=budget.id;
+		}
+  }
+	return budgetId;
+};
+
+async function getCategories(ynabAPI, budget_id) {
+	const response = ynabAPI.categories.getCategories(budget_id)
+	response.then(data => {
+		// console.log(JSON.stringify(data));
+  	const categories = data.data.category_groups;
+		for (let category of categories) {
+		  console.log(`Category id: ${category.id} Name: ${category.name}`);
+	  }
+	});
+};
+
+async function getAccounts(ynabAPI, budget_id) {
+	const response = ynabAPI.accounts.getAccounts(budget_id)
+	response.then(data => {
+  	const accounts = data.data.accounts;
+		for (let account of accounts) {
+		  console.log(`Account id: ${account.id} Name: ${account.name}`);
+	  }
+	});
+};
+
+const ynab = require("ynab");
+const token = require("./config-ynab");
+const ynabAPI = new ynab.API(token.accessToken);
+const response = getBudgetId(ynabAPI);
+response.then(budgetId => {
+  console.log(`after then: budgetId=${budgetId}`);
+  getAccounts(ynabAPI, budgetId);
+	getCategories(ynabAPI, budgetId);
+});
+
